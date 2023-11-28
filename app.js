10158efe5812991e0258142d2aee283dc69711e6
prasad <prasadkanuri25@gmail.com>
2023-11-28 15:33:23 +0000

    API1,API2,API4,API7 not working

--- app.js
+++ app.js
@@ -23,38 +23,18 @@ const initializeDbAndServer = async () => {
   }
 };
 initializeDbAndServer();
-
-const convertPlayerDBObjectTOResponseObject = (dbObject) => {
-  return {
-    playerId: dbObject.player_id,
-    playerName: dbObject.player_name,
-  };
-};
-
-const convertMatchDBObjectToResponseObject = (dbObject) => {
-  return {
-    matchId: dbObject.match_id,
-    match: dbObject.match,
-    year: dbObject.year,
-  };
-};
-
 //get player details
 app.get("/players/", async (request, response) => {
-  const getPlayersQuery = `SELECT * FROM player_details WHERE player_id=${playerId}`;
+  const getPlayersQuery = `SELECT * FROM player_details`;
   const getPlayers = await db.all(getPlayersQuery);
-  response.send(
-    getPlayers.map((echPlayer) =>
-      convertPlayerDBObjectTOResponseObject(eachPlayer)
-    )
-  );
+  response.send(getPlayers);
 });
 //get player Details based on playerId
 app.get("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const getSinglePlayersQuery = `SELECT * FROM player_details WHERE player_id=${playerId}`;
   const getSinglePlayer = await db.get(getSinglePlayersQuery);
-  response.send(convertPlayerDBObjectTOResponseObject(getSinglePlayer));
+  response.send(getSinglePlayer);
 });
 
 //get update player details
@@ -116,13 +96,13 @@ app.get("/players/:playerId/playerScores", async (request, response) => {
             player_details.player_id AS playerId,
             player_details.player_name AS playerName,
             SUM(player_match_score.score) AS totalScore,
-            SUM(player_match_score.fours) AS totalFours,
-            SUM(player_match_score.sixes) AS totalSixes
+            SUM(fours) AS totalFours,
+            SUM(sixes) AS totalSixes
         FROM
-            player_details INNER JOIN player_match_score ON player_details.player_id = player_match_score.player_id
+            player_details INNER JOIN player_match_score ON
+    player_details.player_id = player_match_score.player_id
         WHERE
-            player_id=${playerId};
-            `;
+            player_details.player_id = ${playerId};`;
   const playerScore = await db.all(api7Query);
   response.send(playerScore);
 });
